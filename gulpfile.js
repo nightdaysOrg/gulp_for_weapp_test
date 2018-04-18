b3b34b1bea0cfdbb5d086f8d5916fc876a573fde
hzCat <haizhoucat@qq.com>
2018-04-18 15:51:13 +0800

    错误处理

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ let cssmin = require("gulp-clean-css");
 let jsonmin = require("gulp-jsonminify2");
 let imagemin = require("gulp-imagemin");
 let plumber = require("gulp-plumber");
+let notify = require("gulp-notify");
 
 // 文件夹路径
 let base = "kfy";
@@ -19,52 +20,62 @@ let json = [base + "/*.json", base + "/**/*.json"];
 let image = [base + "/**/*.png", base + "/**/*.jpg", base + "/**/*.jpeg"];
 
 //执行所有
-gulp.task("default", ["js", "css", "html", "json", "image"], function() {});
-gulp.task("js", function() {
+gulp.task("default", ["js", "css", "html", "json", "image"], function () {});
+gulp.task("js", function () {
   return gulp
     .src(js)
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(babel())
     .pipe(uglify())
     .pipe(debug())
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("html", function() {
+gulp.task("html", function () {
   return (
     gulp
-      .src(html)
-      .pipe(plumber())
-      // .pipe(htmlmin())
-      .pipe(gulp.dest("dist"))
+    .src(html)
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
+    // .pipe(htmlmin())
+    .pipe(gulp.dest("dist"))
   );
 });
 
-gulp.task("css", function() {
+gulp.task("css", function () {
   return gulp
     .src(css)
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(cssmin())
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("json", function() {
+gulp.task("json", function () {
   return gulp
     .src(json)
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(jsonmin())
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("image", function() {
+gulp.task("image", function () {
   return gulp
     .src(image)
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: notify.onError("Error: <%= error.message %>")
+    }))
     .pipe(imagemin())
     .pipe(gulp.dest("dist"));
 });
 
-gulp.task("watch", function() {
+gulp.task("watch", function () {
   gulp.watch(js, ["js"]);
   gulp.watch(html, ["html"]);
   gulp.watch(css, ["css"]);
